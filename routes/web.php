c0963affc0611158d44870125151ab1d270471ae
Vu Van Nghia <nghiavu0202@gmail.com>
2024-12-30 22:39:43 +0700

    Build UI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get("/",[PageController::class,'index'])
+    ->name('home');
+Route::get('/works/{cat?}', [PageController::class, 'worksPage'])->name('works');
+
+Route::get('/cat/test',[PageController::class, 'detail'])->name("blog.detail");
+Route::get('/about', function () {
+    return view('pages.about');
+})->name('about');
+
+Route::get('/careers', function () {
+    return view('pages.careers');
+
+})->name('careers');
+
+Route::get('/careers/{page}',[PageController::class,'careerPages'])->name('careers.page');
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+})->name('contact');
+
+Route::post('/contact', [PageController::class, 'sendMessage'])->name('send_message');
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,37 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        'resources/css/themes/default.scss',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+
     ],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: 'var(--color-primary)',
+                normal:'var(--color-text-normal)',
+                // secondary: 'var(--color-secondary)',
+                // warning: 'var(--color-secondary)',
+                // info: 'var(--color-info)',
+                // success: 'var(--color-success)',
+                // danger: 'var(--color-danger)',
+                // error: 'var(--color-error)',
+                default: 'var(--color-default)',
+
+                // muted: "var(--color-text-muted)",
+                // mutedfr: "var(--color-text-muted-fr)",
+                // highlighted: "var(--color-text-highlighted)",
+                // placeholder: "var(--color-text-placeholder)",
+                // divider: "var(--color-divider)",
+                // border: "var(--color-border)",
+                // input: "var(--color-input)",
             },
         },
     },
